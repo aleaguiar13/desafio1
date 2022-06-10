9bda7ef1bc966e57a713dc02e54eaf885f5e5942
aleaguiar13 <costa_alessandra@live.com>
2022-06-10 10:46:56 -0300

    desafio1pronto

---
+++ script.js
@@ -0,0 +1,7 @@
+var nome = "alessandra";//somente o texto precisa de aspas
+var idade = 30;
+var anoQnasci = 1992;
+var quartaFeira = false //o boleano não precisa de aspas, assim como os numeros
+console.log(anoQnasci+idade); //nao precisa colocar aspas porque está mencionando variáveis
+console.log("Eu estou mexendo no console.log"); //para escrever, precisa de aspas
+console.log(quartaFeira);
